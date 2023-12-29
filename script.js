f9ca887a94477470cfea939a90ec6259c637a2f8
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-12-29 13:19:04 +0530

    jokes generator

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const btnEl = document.getElementById("btn");
 const jokeEl = document.getElementById("joke");
+
+const apiKey = "";
+
+async function getJoke(){
+    try {
+        jokeEl.innerText = "";
+        btnEl.disabled = true;
+        btnEl.innerText = "";
+    } catch (error) {
+        jokeEl.innerText = "";
+        btnEl.disabled = false;
+        btnEl.innerText = "";
+        console.log(error);
+    }
+}
+
+btn.addEventListener("click", getJoke);

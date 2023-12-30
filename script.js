3a4d250aae036250a33b9c0682979961fb3f678c
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-12-30 23:43:01 +0530

    joke generator

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const btnEl = document.getElementById("btn");
 const jokeEl = document.getElementById("joke");
 
-const apiKey = "";
+const apiKey = "EgJ0j7/agk3covrNsL1Lfg==NyGXBVCl1Qa9tvaq";
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': apiKey,
+    },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function getJoke(){
     try {
-        jokeEl.innerText = "";
+        jokeEl.innerText = "Updating...";
         btnEl.disabled = true;
-        btnEl.innerText = "";
-    } catch (error) {
-        jokeEl.innerText = "";
+        btnEl.innerText = "Loading...";
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+        
         btnEl.disabled = false;
-        btnEl.innerText = "";
+        btnEl.innerText = "Tell me a joke";
+        
+        jokeEl.innerText = data[0].joke;
+    } catch (error) {
+        jokeEl.innerText = "An error happened, try again later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
         console.log(error);
     }
 }
